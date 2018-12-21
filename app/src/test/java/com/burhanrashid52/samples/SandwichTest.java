72343069a32a9e8afa3f347bf54b40fae0bd0c93
Burhanuddin Rashid <burhanrashid5253@gmail.com>
2018-12-21 14:37:39 +0530

    Added circleci config file

---
+++ app/src/test/java/com/burhanrashid52/samples/SandwichTest.java
@@ -0,0 +1,16 @@
+package com.burhanrashid52.samples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+public class SandwichTest {
+
+    @Test
+    public void sampleTest(){
+        assertEquals(0,0);
+    }
+}
